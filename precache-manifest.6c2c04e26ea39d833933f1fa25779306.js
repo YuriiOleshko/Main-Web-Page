f6afbd7552a85e7c76263f6d8b6aee7324df040e
Yurii <yurii.oleshko@gmail.com>
2019-02-07 00:49:12 +0200

    Updates

---
+++ precache-manifest.6c2c04e26ea39d833933f1fa25779306.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad9ae6914203fd76701f4099eada5ae",
+    "url": "/Main-Web-Page/static/media/weather.7ad9ae69.png"
+  },
+  {
+    "revision": "cb294c5a9a8f32b50b95",
+    "url": "/Main-Web-Page/static/css/main.41015c9f.chunk.css"
+  },
+  {
+    "revision": "d6ce2162b669083611996df2fc741947",
+    "url": "/Main-Web-Page/static/media/ChartLogin.d6ce2162.png"
+  },
+  {
+    "revision": "96fad05dd449a6f84a4d",
+    "url": "/Main-Web-Page/static/js/1.96fad05d.chunk.js"
+  },
+  {
+    "revision": "c9a61f7ed0d05d2a6768",
+    "url": "/Main-Web-Page/static/js/runtime~main.c9a61f7e.js"
+  },
+  {
+    "revision": "1997821cbe9263bd52e15aac09ad7fce",
+    "url": "/Main-Web-Page/static/media/game.1997821c.png"
+  },
+  {
+    "revision": "cb294c5a9a8f32b50b95",
+    "url": "/Main-Web-Page/static/js/main.cb294c5a.chunk.js"
+  },
+  {
+    "revision": "75bd028bb790437b819d692649fb83c0",
+    "url": "/Main-Web-Page/static/media/lp.75bd028b.png"
+  },
+  {
+    "revision": "153b5d1c11f67096aba2f4f8aad00b74",
+    "url": "/Main-Web-Page/static/media/Enshtein.153b5d1c.png"
+  },
+  {
+    "revision": "4a7effcc08118eb69b124b70f3711ad4",
+    "url": "/Main-Web-Page/static/media/chat exit.4a7effcc.png"
+  },
+  {
+    "revision": "24e52befd0982b87b35baf799072ec2f",
+    "url": "/Main-Web-Page/static/media/chatFront.24e52bef.png"
+  },
+  {
+    "revision": "96fad05dd449a6f84a4d",
+    "url": "/Main-Web-Page/static/css/1.2bbf9290.chunk.css"
+  },
+  {
+    "revision": "98773153e9361f3d76d02eef56d9fbad",
+    "url": "/Main-Web-Page/index.html"
+  }
+];
